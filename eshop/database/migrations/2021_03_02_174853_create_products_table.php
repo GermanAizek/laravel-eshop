b4c205d3cf562a8b391ba6a22a3de65df8314daa
GermanAizek <GermanAizek@yandex.ru>
2021-03-02 20:54:32 +0300

    Create models and migrations

---
+++ eshop/database/migrations/2021_03_02_174853_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 100);
+            $table->text('content')->nullable();
+			$table->bigInteger('count')->unsigned()->nullable();
+            $table->decimal('price', 10, 2, true)->default(0);
+			$table->bigInteger('category_id')->unsigned()->nullable();
+			$table->string('image', 50)->nullable();
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                ->references('id')
+                ->on('categories')
+                ->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
